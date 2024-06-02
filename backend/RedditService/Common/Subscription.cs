f47a19713974744b46d88ffd6fcc5a739624ab6a
doram <doramtc8@gmail.com>
2024-06-02 16:40:05 +0200

    Subscription class and its data repository

---
+++ backend/RedditService/Common/Subscription.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Subscription : TableEntity
+    {
+        private int subscription_id;
+        private int user_id;
+        private int topic_id;
+
+        public int Subscription_id { get => subscription_id; set => subscription_id = value; }
+        public int User_id { get => user_id; set => user_id = value; }
+        public int Topic_id { get => topic_id; set => topic_id = value; }
+
+        public Subscription(int subscription_id, int user_id, int topic_id)
+        {
+            this.subscription_id = subscription_id;
+            this.user_id = user_id;
+            this.topic_id = topic_id;
+        }
+
+        public Subscription(string indexNo)
+        {
+            PartitionKey = "Subscription";
+            RowKey = indexNo;
+        }
+        public Subscription()
+        {
+
+        }
+    }
+}
---
+++ backend/RedditService/Common/SubscriptionDataRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class SubscriptionDataRepository
+    {
+        private CloudTable _cloudTable;
+        private CloudStorageAccount _cloudStorageAccount;
+
+        public SubscriptionDataRepository()
+        {
+            _cloudStorageAccount =
+            CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+            CloudTableClient tableClient = new CloudTableClient(new
+            Uri(_cloudStorageAccount.TableEndpoint.AbsoluteUri), _cloudStorageAccount.Credentials);
+            _cloudTable = tableClient.GetTableReference("SubscriptionTable");
+            _cloudTable.CreateIfNotExists();
+        }
+        public IQueryable<Subscription> RetrieveAllSubscriptions()
+        {
+            var results = from g in _cloudTable.CreateQuery<Subscription>()
+                          where g.PartitionKey == "Subscription"
+                          select g;
+            return results;
+        }
+        public void AddSubscription(Subscription newSubscription)
+        {
+            TableOperation insertOperation = TableOperation.Insert(newSubscription);
+            _cloudTable.Execute(insertOperation);
+        }
+
+        public Subscription GetSubscription(string index)
+        {
+            return RetrieveAllSubscriptions().Where(p => p.RowKey == index).FirstOrDefault();
+        }
+
+        public void UpdateSubscription(Subscription subscription)
+        {
+            TableOperation updateOperation = TableOperation.Replace(subscription);
+            _cloudTable.Execute(updateOperation);
+        }
+
+        public List<int> GetTopicByUser(int user_id)
+        {
+            List<Subscription> selectedSubscriptions = RetrieveAllSubscriptions().Where(p => p.User_id == user_id).ToList();
+            List<int> topic = new List<int>();
+            foreach (Subscription s in selectedSubscriptions)
+            {
+                topic.Add(s.Topic_id);
+            }
+            return topic;
+        }
+
+        public void RemoveSubscription(string id)
+        {
+            Subscription subscription = RetrieveAllSubscriptions().Where(s => s.RowKey == id).FirstOrDefault();
+
+            if (subscription != null)
+            {
+                TableOperation deleteOperation = TableOperation.Delete(subscription);
+                _cloudTable.Execute(deleteOperation);
+            }
+        }
+    }
+}
