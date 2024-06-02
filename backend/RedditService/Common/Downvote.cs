f5bc1395061565669c63b1e7f05fe96bb81a731e
doram <doramtc8@gmail.com>
2024-06-02 16:38:10 +0200

    Downvote class with its data repository

---
+++ backend/RedditService/Common/Downvote.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Downvote : TableEntity
+    {
+        private int downvote_id;
+        private int user_id;
+        private int topic_id;
+
+        public int Downvote_id { get => downvote_id; set => downvote_id = value; }
+        public int User_id { get => user_id; set => user_id = value; }
+        public int Topic_id { get => topic_id; set => topic_id = value; }
+
+        public Downvote(int downvote_id, int user_id, int topic_id)
+        {
+            this.downvote_id = downvote_id;
+            this.user_id = user_id;
+            this.topic_id = topic_id;
+        }
+
+        public Downvote(string indexNo)
+        {
+            PartitionKey = "Downvote";
+            RowKey = indexNo;
+        }
+        public Downvote()
+        {
+
+        }
+    }
+}
---
+++ backend/RedditService/Common/DownvoteDataRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class DownvoteDataRepository
+    {
+        private CloudTable _cloudTable;
+        private CloudStorageAccount _cloudStorageAccount;
+
+        public DownvoteDataRepository()
+        {
+            _cloudStorageAccount =
+            CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+            CloudTableClient tableClient = new CloudTableClient(new
+            Uri(_cloudStorageAccount.TableEndpoint.AbsoluteUri), _cloudStorageAccount.Credentials);
+            _cloudTable = tableClient.GetTableReference("DownvoteTable");
+            _cloudTable.CreateIfNotExists();
+        }
+        public IQueryable<Downvote> RetrieveAllDownvotes()
+        {
+            var results = from g in _cloudTable.CreateQuery<Downvote>()
+                          where g.PartitionKey == "Downvote"
+                          select g;
+            return results;
+        }
+        public void AddDownvote(Downvote newDownvote)
+        {
+            TableOperation insertOperation = TableOperation.Insert(newDownvote);
+            _cloudTable.Execute(insertOperation);
+        }
+
+        public Downvote GetDownvote(string index)
+        {
+            return RetrieveAllDownvotes().Where(p => p.RowKey == index).FirstOrDefault();
+        }
+
+        public void UpdateDownvote(Downvote downvote)
+        {
+            TableOperation updateOperation = TableOperation.Replace(downvote);
+            _cloudTable.Execute(updateOperation);
+        }
+
+        public List<int> GetTopicByUser(int user_id)
+        {
+            List<Downvote> selectedDownvotes = RetrieveAllDownvotes().Where(p => p.User_id == user_id).ToList();
+            List<int> topic = new List<int>();
+            foreach (Downvote d in selectedDownvotes)
+            {
+                topic.Add(d.Topic_id);
+            }
+            return topic;
+        }
+
+        public void RemoveDownvote(string id)
+        {
+            Downvote downvote = RetrieveAllDownvotes().Where(s => s.RowKey == id).FirstOrDefault();
+
+            if (downvote != null)
+            {
+                TableOperation deleteOperation = TableOperation.Delete(downvote);
+                _cloudTable.Execute(deleteOperation);
+            }
+        }
+    }
+}
