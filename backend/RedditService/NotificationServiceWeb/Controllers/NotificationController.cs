a9a6337076a5331f9f3ca5c82477471028594119
xmilicapx <milica.pantelic2001@gmail.com>
2024-06-05 17:03:56 +0200

    NotificationServiceWeb

---
+++ backend/RedditService/NotificationServiceWeb/Controllers/NotificationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using NotificationServiceWorker;
+
+public class NotificationController : Controller
+{
+    private CloudTable notificationsLogTable;
+
+    public NotificationController()
+    {
+        
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+        CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+        notificationsLogTable = tableClient.GetTableReference("NotificationTable");
+        notificationsLogTable.CreateIfNotExists();
+    }
+
+    public ActionResult Index()
+    {
+        var logs = GetNotificationLogs();
+        return View(logs);
+    }
+
+    private List<NotificationLogEntity> GetNotificationLogs()
+    {
+        var query = new TableQuery<NotificationLogEntity>();
+        return notificationsLogTable.ExecuteQuery(query).ToList();
+    }
+}
---
+++ backend/RedditService/NotificationServiceWeb/Models/NotificationLog.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotificationServiceWeb.Models
+{
+    public class NotificationLog: TableEntity
+    {
+        public string CommentId { get; set; }
+        public int EmailsSent { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public NotificationLog() { }
+
+        public NotificationLog(string commentId, int emailsSent, DateTime timestamp)
+        {
+            PartitionKey = commentId;
+            RowKey = Guid.NewGuid().ToString();
+            CommentId = commentId;
+            EmailsSent = emailsSent;
+            Timestamp = timestamp;
+        }
+    }
+}
